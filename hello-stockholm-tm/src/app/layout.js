cc8cffff6a241bb1936f27d6e43a46e3d2fab48c
Jonas <jonas.a.teglund@gmail.com>
2024-05-07 19:57:21 +0200

    Dark mode fix

--- hello-stockholm-tm/src/app/layout.js
+++ hello-stockholm-tm/src/app/layout.js
@@ -7,12 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'ÅM',
   description: 'ÅM',
+  icons: {
+    icon: './favicon.ico', // /public path
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <link rel='icon' href='/favicon.ico' />
       <body className={inter.className}>
         <NavBar />
         {children}
