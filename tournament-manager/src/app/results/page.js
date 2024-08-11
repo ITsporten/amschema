30ef9f0e33af78d1a43beb1fa8cd5f32af68849c
Jonas <jonas.a.teglund@gmail.com>
2024-08-11 20:26:15 +0200

    HistoryPage

--- tournament-manager/src/app/results/page.js
+++ tournament-manager/src/app/results/page.js
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react'
 import { db } from '../firebase-config'
 import { collection, getDocs } from 'firebase/firestore'
 import StandingItem from '@/components/StandingItem'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClockRotateLeft, faRightLeft } from '@fortawesome/free-solid-svg-icons';
+import HistoryItem from '@/components/HistoryItem'
 
 export default function Home() {
   const [standingList, setStandingList] = useState([]);
@@ -13,9 +16,16 @@ export default function Home() {
   const [standingList3, setStandingList3] = useState([]);
   const [standingList4, setStandingList4] = useState([]);
   const [season, setSeason] = useState("");
+  const [historyList, setHistoryList] = useState([])
 
+  const [currentSeason, setCurrentSeason] = useState(true);
   const [filter, setFilter] = useState("Total")
   const standingsCollectionRef = collection(db, "Standings");
+  const historyCollectionRef = collection(db, "History");
+
+  const toggleSeason = () => {
+    setCurrentSeason(!currentSeason);
+  }
 
   const compTotal = (standing1, standing2) => {
     if(standing1.TotalPoints <= standing2.TotalPoints){
@@ -56,6 +66,16 @@ export default function Home() {
       return -1;
     }
   }
+
+  const compHistory = (history1, history2) => {
+    const h1 = parseInt(history1.Season.slice(-2));
+    const h2 = parseInt(history2.Season.slice(-2));
+    if(h1 <= h2){
+      return 1;
+    }else{
+      return -1;
+    }
+  }
   
   useEffect(() => {
     const getStandings = async () => {
@@ -89,53 +109,88 @@ export default function Home() {
       setSeason(lst[0].Season);
     }
     
+    const getHistory = async () => {
+      const data = await getDocs(historyCollectionRef);
+      let lst = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      lst.sort(compHistory);
+      setHistoryList(lst)
+    }
     getStandings();
+    getHistory();
   }, [])
 
   return (
     <main className={styles.main}>
-      <h1 className={styles.standingHeader}>{season}</h1>
-      <select value={filter} onChange={e => setFilter(e.target.value)}>
-          <option value="Total">Säsongsställning</option>
-          <option value="Volley">Volleyboll</option>
-          <option value="Surprise">Surprise</option>
-          <option value="Floorball">Innebandy</option>
-          <option value="Football">Fotboll</option>
-      </select>
-      <div className={styles.teamListContainer}>
-        { filter == "Total" ?
-          <>
-            {standingList.map((standing, index) => 
-              <StandingItem key={standing.id} standingItem={standing} points={standing.TotalPoints} placement={index+1}/>)
+      { currentSeason &&
+        <>
+          <h1 className={styles.standingHeader}>{season}</h1>
+          <div className={styles.clockFilterContainer}>
+            <div className={styles.historyButton} onClick={toggleSeason}>
+              <FontAwesomeIcon className={styles.icon} icon={faRightLeft} />
+              <h4 className={styles.historyButtonText}>Historik</h4>
+            </div>
+            <select className={styles.standingFilter} value={filter} onChange={e => setFilter(e.target.value)}>
+                <option value="Total">Säsongsställning</option>
+                <option value="Volley">Volleyboll</option>
+                <option value="Surprise">Surprise</option>
+                <option value="Floorball">Innebandy</option>
+                <option value="Football">Fotboll</option>
+            </select>
+          </div>
+          <div className={styles.teamListContainer}>
+            { filter == "Total" ?
+              <>
+                {standingList.map((standing, index) => 
+                  <StandingItem key={standing.id} standingItem={standing} points={standing.TotalPoints} placement={index+1}/>)
+                }
+              </> 
+              : filter == "Volley" ?
+              <>
+                {standingList1.map((standing, index) => 
+                  <StandingItem key={standing.id} standingItem={standing} points={standing.Points[0]} placement={index+1}/>)
+                }
+              </>
+              : filter == "Surprise" ?
+              <>
+                {standingList2.map((standing, index) => 
+                  <StandingItem key={standing.id} standingItem={standing} points={standing.Points[1]} placement={index+1}/>)
+                }
+              </>
+              : filter == "Floorball" ? 
+              <>
+                {standingList3.map((standing, index) => 
+                  <StandingItem key={standing.id} standingItem={standing} points={standing.Points[2]} placement={index+1}/>)
+                } 
+              </>
+              : 
+              
+              <>
+                {standingList4.map((standing, index) => 
+                  <StandingItem key={standing.id} standingItem={standing} points={standing.Points[3]} placement={index+1}/>)
+                } 
+              </>
             }
-          </> 
-          : filter == "Volley" ?
-          <>
-            {standingList1.map((standing, index) => 
-              <StandingItem key={standing.id} standingItem={standing} points={standing.Points[0]} placement={index+1}/>)
+          </div>
+        </>
+      }
+      { !currentSeason &&
+        <>
+          <h1 className={styles.standingHeader}>Historik</h1>
+            <div className={styles.historyButton} onClick={toggleSeason}>
+              <FontAwesomeIcon className={styles.icon} icon={faRightLeft} />
+              <h4 className={styles.historyButtonText}>Årets säsong</h4>
+              
+            </div>
+            {historyList.length != 0 &&
+            <>
+              { historyList.map((history, index) => 
+                <HistoryItem key={history.id} historyItem={history}/>)
+              }
+            </>
             }
-          </>
-          : filter == "Surprise" ?
-          <>
-            {standingList2.map((standing, index) => 
-              <StandingItem key={standing.id} standingItem={standing} points={standing.Points[1]} placement={index+1}/>)
-            }
-          </>
-          : filter == "Floorball" ? 
-          <>
-            {standingList3.map((standing, index) => 
-              <StandingItem key={standing.id} standingItem={standing} points={standing.Points[2]} placement={index+1}/>)
-            } 
-          </>
-          : 
-          
-          <>
-            {standingList4.map((standing, index) => 
-              <StandingItem key={standing.id} standingItem={standing} points={standing.Points[3]} placement={index+1}/>)
-            } 
-          </>
-        }
-      </div>
+        </>
+      }
+      
     </main>
   )
 }
--- tournament-manager/src/components/StandingItem.js
+++ tournament-manager/src/components/StandingItem.js
@@ -3,10 +3,10 @@ import style from '../app/page.module.css'
 import Link from 'next/link'
 import { usePathname } from "next/navigation";
 
-export default function StandingItem({standingItem, points, placement}) {
+export default function StandingItem({standingItem, points, placement, history=false}) {
 
     return (
-        <div className={style.standingContainer}>
+        <div className={history ? style.standingContainerHistory: style.standingContainer}>
             <div className={placement == 1 ? style.standingLeftBoxGold : placement == 2 ? style.standingLeftBoxSilver : placement == 3 ? style.standingLeftBoxBronze : style.standingLeftBox}>
                 <h4 className={style.standingText}>{placement}.</h4>
             </div>
@@ -14,7 +14,7 @@ export default function StandingItem({standingItem, points, placement}) {
                 <h4 className={style.standingText}>{standingItem.TeamName}</h4>
             </div>
             <div className={style.standingRightBox}>
-                <h4 className={style.standingText}>{points}</h4>
+                <h4 className={style.standingText}>{points != '-' ? points + 'p' : 'NA'}</h4>
             </div>
         </div>
     )
