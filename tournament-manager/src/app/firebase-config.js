5b5203379e14d1c37c455ee49cb20ae909b52fdf
jteglund <58300973+jteglund@users.noreply.github.com>
2024-08-24 10:57:31 +0200

    Update firebase-config.js

--- tournament-manager/src/app/firebase-config.js
+++ tournament-manager/src/app/firebase-config.js
@@ -3,14 +3,14 @@ import { getFirestore } from "@firebase/firestore"
 import { getAuth } from "@firebase/auth"
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: "angstromsmasterskapet.firebaseapp.com",
-    projectId: "angstromsmasterskapet",
-    storageBucket: "angstromsmasterskapet.appspot.com",
-    messagingSenderId: "612305948170",
-    appId: "1:612305948170:web:9461f9713c12852da919f3",
-    measurementId: "G-0WF5W551Z1"
-  };
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: "amschema-e76e1.firebaseapp.com",
+  projectId: "amschema-e76e1",
+  storageBucket: "amschema-e76e1.appspot.com",
+  messagingSenderId: "668728587864",
+  appId: "1:668728587864:web:8259e9d5e76949725f3c2d",
+  measurementId: "G-7WXQFCRYJ1"
+};
 
 
 const app = initializeApp(firebaseConfig);
